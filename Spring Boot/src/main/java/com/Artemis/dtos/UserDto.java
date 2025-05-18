2787fbada697b4b54ac85969b6eef88420b7ea55
Artem Zakharov <artemzaxarov164@gmail.com>
2025-05-18 22:03:15 +0300

    13 commit

---
+++ Spring Boot/src/main/java/com/Artemis/dtos/UserDto.java
@@ -0,0 +1,19 @@
+package com.Artemis.dtos;
+
+import com.Artemis.enums.UserRole;
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private UserRole userRole;
+
+}
