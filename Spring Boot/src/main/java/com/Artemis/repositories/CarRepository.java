6b91a303e9f60c109a4c6ce60561b31dc7f64bf0
Artem Zakharov <artemzaxarov164@gmail.com>
2025-05-18 22:04:45 +0300

    17 commit

---
+++ Spring Boot/src/main/java/com/Artemis/repositories/CarRepository.java
@@ -0,0 +1,9 @@
+package com.Artemis.repositories;
+
+import com.Artemis.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+}
